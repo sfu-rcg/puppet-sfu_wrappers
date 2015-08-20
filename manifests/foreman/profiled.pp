ded211b9fe65eea1047664c1975f73e2c587aa4e
Adam S <asa188@sfu.ca>
2015-08-19 18:53:54 -0700

    Added profiled before user consul, properly

--- manifests/foreman/profiled.pp
+++ manifests/foreman/profiled.pp
@@ -3,7 +3,7 @@
 class sfu_wrappers::foreman::profiled($configs = {}) {
   validate_hash($configs)
   if !empty($configs) {
-    Class['sfu_wrappers::foreman::profiled'] -> User<| $title == 'consul' |>
+    Profiled::Script<| |> -> User<| $title == 'consul' |>
     create_resources('::profiled::script', $configs)
   }
 }
